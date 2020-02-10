7d94b0f3357015d10d32ee3180367a82363c43fd
ArtemProgrammist <artemis09035@gmail.com>
2020-02-10 20:00:23 +0700

    Initial commit

---
+++ src/kkk.kt
@@ -0,0 +1,28 @@
+
+class kvantorium{
+    var one:Array<kvantum> = arrayOf()
+
+} class kvantum{
+    var two:Array<group> = arrayOf()
+
+}class group{
+    var three:Array<student> = arrayOf()
+
+} class student{
+  var four:Array<pupil> = arrayOf()
+}
+class pupil{
+    var name =
+    var surname =
+    var middle =
+    var birthday =
+}
+class teacher{
+    var name1 =
+    var surname1 =
+    var middle1 =
+    var birthday1 = 
+}
+fun main(){
+    var
+}
